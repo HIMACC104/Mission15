7595df13a99696b6af390321a6d7d8d153981612
HIMAcc104 <a1070232@gmail.com>
2019-01-07 16:35:49 +0800

    vMotion

---
+++ web/app.py
@@ -0,0 +1,13 @@
+"""
+flask 第一版
+"""
+
+from flask import Flask, request, abort
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def hello_world():
+    return 'This is version vMotion20190107.'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000)
