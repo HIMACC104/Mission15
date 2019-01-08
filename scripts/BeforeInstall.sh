b02ddccd13bf94cb318868ca74554b0b1d245e55
HIMAcc104 <a1070232@gmail.com>
2019-01-08 10:07:42 +0800

    ver2.0

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -2,7 +2,7 @@
 
 $(aws ecr get-login --no-include-email --registry-ids 129729052534 --region ap-northeast-1)
 
-if [ `docker images | grep cc104-web-student18 | wc -l`  = 1 ]
+if [ `docker images | grep flask-web | wc -l`  = 1 ]
 then
         docker rmi cc104-web-student18
         docker pull 129729052534.dkr.ecr.ap-northeast-1.amazonaws.com/cc104-web-student18:latest
